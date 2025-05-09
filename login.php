148bf4ab8a36e465e80edbfb9d2ac820f8642e1f
Jagasaki69 <fireandice384@gmail.com>
2025-05-09 14:46:23 +0700

    login

--- login.php
+++ login.php
@@ -33,6 +33,7 @@ if (isset($_POST["login"])) {
 
 <!DOCTYPE html>
 <html lang="en">
+    
     <head>
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -40,40 +41,51 @@ if (isset($_POST["login"])) {
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>Login</title>
-        <link href="css/styles.css" rel="stylesheet" />
+        <link href="css/login.css" rel="stylesheet" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
     </head>
-    <body class="bg-primary">
-        <div id="layoutAuthentication">
-            <div id="layoutAuthentication_content">
-                <main>
-                    <div class="container">
-                        <div class="row justify-content-center">
-                            <div class="col-lg-5">
-                                <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Login</h3></div>
-                                    <div class="card-body">
-                                        <form method="post">
-                                            <div class="form-group">
-                                                <label class="small mb-1" for="inputEmailAddress">Email</label>
-                                                <input class="form-control py-4" name="email" id="inputEmailAddress" type="email" placeholder="Enter email address" />
-                                            </div>
-                                            <div class="form-group">
-                                                <label class="small mb-1" for="inputPassword">Password</label>
-                                                <input class="form-control py-4" name="password" id="inputPassword" type="password" placeholder="Enter password" />
-                                            </div>
-                                            <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0">
-                                                <button class="btn btn-primary" name="login" href="index.html">Login</button>
-                                            </div>
-                                        </form>
-                                    </div>
+    <body>
+    <div id="layoutAuthentication">
+        <div id="layoutAuthentication_content">
+            <main>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-5">
+                            <div class="card">
+                                <div class="logo-container">
+                                    <img src="assets/img/logobpbd.jpg" alt="Logo BPBD">
+                                </div>
+                               
+                                <div class="card-body">
+                                    <form method="post">
+                                        <div class="form-group">
+                                            <label class="form-label" for="inputEmailAddress">
+                                                <i class="fas fa-envelope login-icon"></i>Email
+                                            </label>
+                                            <input class="form-control" name="email" id="inputEmailAddress" 
+                                                   type="email" placeholder="Enter your email" required />
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="form-label" for="inputPassword">
+                                                <i class="fas fa-lock login-icon"></i>Password
+                                            </label>
+                                            <input class="form-control" name="password" id="inputPassword" 
+                                                   type="password" placeholder="Enter your password" required />
+                                        </div>
+                                        <div class="form-group">
+                                            <button class="btn btn-primary" name="login" type="submit">
+                                                <i class="fas fa-sign-in-alt mr-2"></i>Login
+                                            </button>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </main>
-            </div>
+                </div>
+            </main>
         </div>
+    </div>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="js/scripts.js"></script>
